c51e3e7a5e0a7fd4b46d88b0f2fae860fd9a245d
upendrabhatt2607 <upendrabhatt2607@gmail.com>
2023-10-18 18:47:11 +0530

    HTML & CSS Projects are added.

---
+++ profile_card/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Card UI Design</title>
+   
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    
+    <div class="profile-card">
+        <div class="image">
+            <img src="sammy.jpg" alt="SammyJohn" class="profile-img">
+        </div>
+
+        <div class="text-data">
+            <span class="name">Sanyam Ghale</span>
+            <span class="job">D𝒊𝒓𝒆𝒄𝒕 & P𝒓𝒐𝒅𝒖𝒄𝒆</span>
+            <span class="job">At Scimitar Entertainment</span>
+
+        </div>
+
+        <div class="media-button">
+            <a href="https://www.facebook.com/sanyam.ghale" style="background: #4267b2" class="link">
+                <i class='bx bxl-facebook'></i>
+            </a>
+            <a href="https://www.instagram.com/sammyjohn.316/" style="background: #e1306c" class="link">
+                <i class='bx bxl-instagram-alt' ></i>
+            </a>
+            <a href="https://www.youtube.com/@sammyjohn.production4783" style="background: #ff0000" class="link">
+                <i class='bx bxl-youtube' ></i>
+            </a>
+            <a href="https://www.tiktok.com/@sammy_john3.16" style="background: #000" class="link">
+                <i class='bx bxl-tiktok' ></i>
+            </a>
+        </div>
+
+        <div class="buttons">
+            <button class="btn">Subscribe</button>
+            <button class="btn">Message </button>
+        </div>
+
+        <div class="analytics">
+            <div class="data">
+                <i class='bx bx-heart'></i>
+                <span class="number">23K</span>
+            </div>
+            <div class="data">
+                <i class='bx bx-message-rounded'></i>
+                <span class="number">30K</span>
+            </div>
+            <div class="data">
+                <i class='bx bx-share' ></i>
+                <span class="number">50K</span>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
